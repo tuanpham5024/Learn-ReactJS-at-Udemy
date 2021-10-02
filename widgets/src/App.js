40aaa176b05fe6c6595ae96ade37948612b6e2a4
Pham Anh Tuan <tuanpham5024@gmail.com>
2021-10-02 07:42:54 +0700

    Navigation

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -3,7 +3,8 @@ import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
-
+import Route from "./components/Route";
+import Header from "./components/Header";
 const items = [
   {
     title: "What is React?",
@@ -34,10 +35,29 @@ const options = [
   },
 ];
 
+
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
-      <Translate />
+      <Header /> 
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route> 
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 };
